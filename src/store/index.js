47426574aaaa89b0878bd45a4c9fcb5f3f9fea0d
LGMarques9963 <lorran9963@gmail.com>
2023-07-19 19:10:05 -0300

    Inital commit

--- src/store/index.js
+++ src/store/index.js
@@ -4,9 +4,15 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    token: localStorage.getItem('token') || '',
+  },
   getters: {},
-  mutations: {},
+  mutations: {
+    setToken(state, token) {    
+      state.token = token
+    }
+  },
   actions: {},
   modules: {},
 });
